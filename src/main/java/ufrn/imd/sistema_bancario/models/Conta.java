d51b62567c80e10291716c7129e39cbfae15f9a8
Georg Herison Maia Nunes <georg.maia.700@ufrn.edu.br>
2026-07-09 22:05:14 -0300

    feat: adiciona contratos JML ao model Conta

--- src/main/java/ufrn/imd/sistema_bancario/models/Conta.java
+++ src/main/java/ufrn/imd/sistema_bancario/models/Conta.java
@@ -7,14 +7,27 @@ import ufrn.imd.sistema_bancario.services.exceptions.ValorInvalidoException;
 @Getter
 @Setter
 public class Conta {
+    //@ spec_public
     private String numero;
+    //@ spec_public
     private double saldo;
 
+    //@ public invariant saldo >= 0
+    //@ public invariant numero != null && !numero.trim().isEmpty()
+
+    //@ requires numero != null && !numero.trim().isEmpty()
+    //@ assignable numero, saldo
+    //@ ensures this.numero.equals(numero)
+    //@ ensures this.saldo == 0
     public Conta(String numero) {
         this.numero = numero;
         this.saldo = 0;
     }
 
+    //@ requires valor > 0
+    //@ assignable saldo
+    //@ ensures saldo == \old(saldo) + valor
+    //@ ensures saldo > \old(saldo)
     public void creditar(double valor) {
         if (valor <= 0) {
             throw new ValorInvalidoException();
@@ -22,6 +35,10 @@ public class Conta {
         this.saldo += valor;
     }
 
+    //@ requires valor > 0 && saldo >= valor
+    //@ assignable saldo
+    //@ ensures saldo == \old(saldo) - valor
+    //@ ensures saldo < \old(saldo)
     public void debitar(double valor) {
         if (valor <= 0) {
             throw new ValorInvalidoException();
